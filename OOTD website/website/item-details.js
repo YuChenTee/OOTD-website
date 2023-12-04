dfea23171748688052500cb978e461d811dffe42
Tee Yu Chen <86150851+CyrusT1221@users.noreply.github.com>
2023-12-04 12:33:24 +0800

    Add files via upload

---
+++ OOTD website/website/item-details.js
@@ -0,0 +1,358 @@
+const itemDetails = [
+  { 
+    id: 'item1',
+    gender: 'Women',
+    image: 'images/image1.png',
+    name: 'Trim Crop Sleeve Blouse',
+    price: 'RM 50',
+    category: 'Top',
+    description: 'Tight and brilliant design provide stunning look. Comfortable wear made from recycled polyester. ',
+  },
+  {
+    id: 'item2',
+    gender: 'Women',
+    image: 'images/image2.png',
+    name: 'Geometric Frame Boho Fashion Glasses',
+    price: 'RM 100',
+    category: 'Accessories',
+    description: 'Fashion glasses provide gorgeous look during summer. ',
+  },
+  {
+    id: 'item3',
+    gender: 'Women',
+    image: 'images/image3.png',
+    name: 'Women Solid Square Buckle Casual Belt',
+    price: 'RM 40',
+    category: 'Accessories',
+    description: 'Casual wear belt outstand body shape. ',
+  },
+    {
+      id: 'item4',
+      gender: 'Men',
+      image: 'images/image4.png',
+      name: 'Oversized Pocket Crew Neck Half Sleeve T-Shirt',
+      price: 'RM 40',
+      category: 'Top',
+      description: '100% cotton T-shirt, pre-washed and plain design for a casual look. Oversized cut with a pocket. ',
+    },
+    {
+      id: 'item5',
+      gender: 'Men',
+      image: 'images/image5.png',
+      name: 'Cotton Broadcloth Check Short Sleeve Shirt',
+      price: 'RM 60',
+      category: 'Top',
+      description: 'Premium cotton shirt made from 100% premium ultra-long fibres provides high quality feels for comfortable wear. ',
+    },
+    {
+      id: 'item6',
+      gender: 'Men',
+      image: 'images/image6.png',
+      name: 'Cotton Broadcloth Check Long Sleeve Shirt',
+      price: 'RM 70',
+      category: 'Top',
+      description: 'Premium cotton shirt made from breathable cotton fibres. Stunning colours and an exquisite texture ',
+    },
+    {
+      id: 'item7',
+      gender: 'Men',
+      image: 'images/image7.png',
+      name: 'Crew Neck Short Sleeve T-Shirt',
+      price: 'RM 30',
+      category: 'Top',
+      description: 'Made from 100% recycled polyester provide a smooth and cool feel.',
+    },
+    {
+      id: 'item8',
+      gender: 'Men',
+      image: 'images/image8.png',
+      name: 'Smart Ankle Pants',
+      price: 'RM 60',
+      category: 'Bottoms',
+      description: 'Sleek and stetchy Pants with 2-way stretch fabric. Wrinkle-resistant, easy care after washing. ',
+    },
+    {
+      id: 'item9',
+      gender: 'Men',
+      image: 'images/image9.png',
+      name: 'Sweat Pants',
+      price: 'RM 80',
+      category: 'Bottoms',
+      description: 'Soft and comfortable sweatpants made from ribbed fabric for a good fit and softness. ',
+    },
+    {
+      id: 'item10',
+      gender: 'Men',
+      image: 'images/image10.png',
+      name: 'Stretch Active Shorts',
+      price: 'RM 60',
+      category: 'Bottoms',
+      description: 'Shorts made from Inner mesh fabric for awesome breathability. Ultra Stretch fabric for easy movement. ',
+    },
+    {
+      id: 'item11',
+      gender: 'Women',
+      image: 'images/image11.png',
+      name: 'Solid Pleated Detail Wide Leg Shorts',
+      price: 'RM 45',
+      category: 'Bottoms',
+      description: 'Made from Woven Fabric provide excellent experience and outfit. ',
+    },
+    {
+      id: 'item12',
+      gender: 'Women',
+      image: 'images/image12.png',
+      name: 'Sweetheart Neck Ruched Crop ',
+      price: 'RM 55',
+      category: 'Top',
+      description: 'Casual wear but outstanding outfit with stunning yellow colour. ',
+    },
+    {
+      id: 'item13',
+      gender: 'Women',
+      image: 'images/image13.png',
+      name: 'Solid Flounce Long Sleeve Crop Tee',
+      price: 'RM 55',
+      category: 'Top',
+      description: 'Made from Knitted Fabric which enhanced breathability provide extreme comfortable wear. ',
+    },
+    {
+      id: 'item14',
+      gender: 'Women',
+      image: 'images/image14.png',
+      name: 'Belted Shorts ',
+      price: 'RM 65',
+      category: 'Bottoms',
+      description: 'Give a classic look with belt which distinguish from others.  ',
+    },
+
+    {
+      id: 'item15',
+      gender: 'Women',
+      image: 'images/image15.png',
+      name: 'Drawstring Waist Shorts',
+      price: 'RM 25',
+      category: 'Bottoms',
+      description: 'Made from Kinnited Fabric. Ultra stretch fabric for easy movement',
+    },
+    {
+      id: 'item16',
+      gender: 'Kids',
+      image: 'images/image16.png',
+      name: 'Cotton Short Sleeve T-shirt & Shorts',
+      price: 'RM 55',
+      category: 'Bottoms',
+      description: 'Made from Ultra Stretch fabric for easy movement greater comfort',
+    },
+    {
+      id: 'item17',
+      gender: 'Kids',
+      image: 'images/image17.png',
+      name: 'Open Collar Short Sleeve Shirt & Shorts',
+      price: 'RM 75',
+      category: 'Bottoms',
+      description: 'Premium cotton shirt made from breathable cotton fibres. Stunning colours and an exquisite texture ',
+    },
+    {
+      id: 'item18',
+      gender: 'Kids',
+      image: 'images/image18.png',
+      name: 'Hooded Neck Tee & Shorts',
+      price: 'RM 45',
+      category: 'Bottoms',
+      description: 'Pill-resistant 100% cotton. Smoot and stretchy for high performance comfort. ',
+    },
+    {
+      id: 'item19',
+      gender: 'Kids',
+      image: 'images/image19.png',
+      name: 'Cotton Graphic Pocket Patched Shirt Without Tee',
+      price: 'RM 65',
+      category: 'Outwear',
+      description: 'Stunning letter logo design with comfortable wear ',
+    },
+    {
+      id: 'item20',
+      gender: 'Kids',
+      image: 'images/image20.png',
+      name: 'Kids Graphic Tee ',
+      price: 'RM 65',
+      category: 'Top',
+      description: 'Suitable for casual wear with meaningful design ',
+    },
+    {
+      id: 'item21',
+      gender: 'Kids',
+      image: 'images/image21.png',
+      name: 'Kids Cartoon Graphic Tee',
+      price: 'RM 65',
+      category: 'Top',
+      description: 'Casual wear. Cartoon design which loved by kids',
+    },
+    {
+      id: 'item22',
+      gender: 'Kids',
+      image: 'images/image22.png',
+      name: 'Kids Girl Strawberry Pattern Drop Shoulder Cardigan ',
+      price: 'RM 35',
+      category: 'Outwear',
+      description: '100% cotton provide comfortable wear. ',
+    },
+    {
+      id: 'item23',
+      gender: 'Kids',
+      image: 'images/image23.png',
+      name: 'Kids Girl Flap Pocket Ruffle Hem Denim Dress',
+      price: 'RM 65',
+      category: 'Top',
+      description: 'Traditional styled dress. Made by fabric cotton. ',
+    },
+    {
+      id: 'item24',
+      gender: 'Deals',
+      image: 'images/image24.png',
+      name: 'Solid Round Neck Tee',
+      price: 'RM 25',
+      category: 'Top',
+      description: 'Casual wear. Made by cotton provide breathable and comfortable wear. ',
+    },
+    {
+      id: 'item25',
+      gender: 'Deals',
+      image: 'images/image25.png',
+      name: 'Letter Patched Detail Flap Pocket Drawstring Waist Cargo Shorts',
+      price: 'RM 45',
+      category: 'Bottoms',
+      description: 'Layered pocket provide outstanding look ',
+    },
+    {
+      id: 'item26',
+      gender: 'Deals',
+      image: 'images/image26.png',
+      name: 'Flap Pocket Side Cargo Pants ',
+      price: 'RM 35',
+      category: 'Bottoms',
+      description: 'Old fashion cargo pants. Stunning fashion. ',
+    },
+    {
+      id: 'item27',
+      gender: 'Deals',
+      image: 'images/image27.png',
+      name: 'Letter Graphic Stripped Trim Short ',
+      price: 'RM 15',
+      category: 'Bottoms',
+      description: 'Shorts design with sports element provide sporty look',
+    },
+    {
+      id: 'item28',
+      gender: 'Deals',
+      image: 'images/image28.png',
+      name: 'Solid Waffle Knif Polo Shirt',
+      price: 'RM 50',
+      category: 'Top',
+      description: 'Polo shirt provide polite look ',
+    },
+    {
+      id: 'item29',
+      gender: 'Men',
+      image: 'images/image29.png',
+      name: 'Two Tone Varsity Jacket',
+      price: 'RM 100',
+      category: 'Outwear',
+      description: 'Make with 100% cotton. Keeping warmth form cold. ',
+    },
+    {
+      id: 'item30',
+      gender: 'Men',
+      image: 'images/image30.png',
+      name: 'Cartoon Bear Jacket',
+      price: 'RM 120',
+      category: 'Outwear',
+      description: '100% cotton. Cutie fashion look',
+    },
+    {
+      id: 'item31',
+      gender: 'Men',
+      image: 'images/image31.png',
+      name: 'Men Geometric Pattern Slippers For Indoor',
+      price: 'RM 50',
+      category: 'Accessories',
+      description: 'Comforable wear. Waterproof materials.',
+    },
+    {
+      id: 'item32',
+      gender: 'Men',
+      image: 'images/image32.png',
+      name: 'Men Black Square Buckle Belt',
+      price: 'RM 30',
+      category: 'Accessories',
+      description: 'High quality leather belts which provide high-class look. ',
+    },
+    {
+      id: 'item33',
+      gender: 'Men',
+      image: 'images/image33.png',
+      name: 'Embroidery Baseball Cap',
+      price: 'RM 40',
+      category: 'Accessories',
+      description: 'Caps designed with letter and cute cartoon which provide sporty look. ',
+    },
+    {
+      id: 'item34',
+      gender: 'Women',
+      image: 'images/image34.png',
+      name: 'Quilted Detail Baguette Bag',
+      price: 'RM 70',
+      category: 'Accessories',
+      description: 'Geometric design suitable for high-class look. ',
+    },
+
+  ]
+
+var navigationSelection = sessionStorage.getItem('navigation');
+
+ // initialize storage 
+if (!sessionStorage.getItem('category')){
+  sessionStorage.setItem('category', "All");
+ }
+ else{
+  var categorySelection = sessionStorage.getItem('category');
+ }
+
+ if (!sessionStorage.getItem('min-price')){
+  sessionStorage.setItem('min-price', 0);
+ }
+ else{
+  var minPrice = sessionStorage.getItem('min-price');
+ }
+
+ if (!sessionStorage.getItem('max-price')){
+  sessionStorage.setItem('max-price', 9999);
+ }
+ else{
+  var maxPrice = sessionStorage.getItem('max-price');
+ }
+
+ if(!sessionStorage.getItem('cart_array')){
+  const cart_array = [];
+  const cart_array_String = JSON.stringify(cart_array);
+
+  const size_array = [];
+  const size_array_String = JSON.stringify(size_array);
+
+  const quantity_array = [];
+  const quantity_array_String = JSON.stringify(quantity_array);
+
+  sessionStorage.setItem('cart_array',cart_array_String);
+  sessionStorage.setItem('size_array',size_array_String);
+  sessionStorage.setItem('quantity_array',quantity_array_String);
+}
+
+ function navigate(x) {
+  sessionStorage.setItem('navigation', x);
+}
+
+function set_current_browsing_item(x){
+  var current_browsing_item = sessionStorage.setItem('current-item',x);
+}
+
